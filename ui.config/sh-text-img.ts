2e0c49bee8314b4f7e1845a0cdb69f9b7814e5f1
Joaquin Prado <joaquin@elastichub.io>
2025-02-22 13:08:15 -0800

    udpate image size

--- ui.config/sh-text-img.ts
+++ ui.config/sh-text-img.ts
@@ -4,7 +4,7 @@ export default {
   underline: "border-b-[3px] border-neutral-500 dark:border-oma-yellow-500 rounded-xl w-32 -mt-2",
   subtitle: "subtitle font-semibold dark:saturate-[180%] text-lg sm:text-[24px] md:text-[24px] lg:text-[24px] break-words -mt-8",
   text: "not-prose text-lg sm:text-md md:text-lg lg:text-xl leading-loose break-words mt-16",
-  image: "mx-auto rounded-xl max-w-[250px] sm:max-w-[300px] md:max-w-[400px] lg:max-w-[500px]",
+  image: "mx-auto rounded-xl",
   status: "",
   // Default Tailwind CSS values
   default: { 
