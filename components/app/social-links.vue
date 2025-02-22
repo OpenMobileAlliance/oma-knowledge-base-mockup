929d5fdb65fc7d891a3f17a04ccde0fc0ff565d7
Filip Dragović <dr.ipi95@gmail.com>
2025-02-22 12:59:42 +0100

    update: color of icons changed

--- components/app/social-links.vue
+++ components/app/social-links.vue
@@ -2,7 +2,7 @@
   <ul class="flex mt-4 sm:justify-center sm:mt-0">
     <li v-for="socialLink of socialLinks" :key="socialLink.title">
       <ULink :to="socialLink.url" :title="socialLink.title" target="_blank" active-class="text-primary"
-        class="hover:text-oma-blue-500 font-extralight text-slate-400 ml-1">
+        class="font-extralight text-slate-600 dark:text-golden hover:text-oma-yellow-700 dark:hover:text-oma-yellow-500 ml-1">
         <UIcon :name="socialLink.logo" />
       </ULink>
     </li>
