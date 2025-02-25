1aef18512c60e3966cc2fbe8328cd018ad302a88
Filip Dragović <dr.ipi95@gmail.com>
2025-02-25 16:01:29 +0100

    update: issue resolved

--- components/content/sh-helper-component.vue
+++ components/content/sh-helper-component.vue
@@ -1,18 +1,45 @@
 <template>
-  <div>
-    <div class="flex-col">
-      <div class="mb-5 mx-auto">
-        <img src="/images/oma2.png" class="m-0 max-w-full max-h-full object-contain" />
+  <div :class="ui.wrapper">
+    <div :class="ui.inner">
+      <div :class="ui.firstLogo">
+        <img v-if="computedLogoSrc && firstLogoDark" :src="firstLogoDark" :class="ui.firstLogoDark" />
+        <img v-if="!computedLogoSrc && firstLogoLight" :src="firstLogoLight" :class="ui.firstLogoLight" />
       </div>
-      <div class="">
-        <img v-if="!computedLogoSrc" src="/logo-light.png" class="m-0 max-w-full max-h-full object-contain" />
-        <img v-if="computedLogoSrc" src="/logo-dark.png" class="m-0 max-w-full max-h-full object-contain" />
+      <div :class="ui.secondLogo">
+        <img v-if="computedLogoSrc && secondLogoDark" :src="secondLogoDark" :class="ui.secondLogoDark" />
+        <img v-if="!computedLogoSrc && secondLogoDark" :src="secondLogoLight" :class="ui.secondLogoLight" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { helperComponent as config } from '@/ui.config' // Import the config file
+
+const props = withDefaults(
+  defineProps<{
+    description?: string;
+    firstLogoDark?: string;
+    firstLogoLight?: string;
+    secondLogoDark?: string;
+    secondLogoLight?: string;
+    ui?: Partial<typeof config>;
+  }>(),
+  {
+    ui: () => ({}),
+    firstLogoDark: "/logo-dark.png",
+    firstLogoLight: "/logo-light.png",
+    secondLogoDark: "/images/oma2.png",
+    secondLogoLight: "",
+    description: "",
+  }
+);
+
+const { ui } = useUI(
+  "sh-helper-component",
+  toRef(props, "ui"),
+  config
+);
 
 const theme = useColorMode();
 
