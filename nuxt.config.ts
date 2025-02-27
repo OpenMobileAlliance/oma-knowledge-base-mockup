96c5c8aca92f16078883dad74c6b06f09edb2587
Filip Dragović <dr.ipi95@gmail.com>
2025-02-27 17:54:29 +0100

    update: prerender media/articles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,7 +67,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ['/news', '/news/articles', '/guidelines', '/groups', '/media', '/oma-events', '/about/faq/', '/oma-events/past-events', '/about/newsletter'],
+      routes: ['/news', '/media/articles', '/guidelines', '/groups', '/media', '/oma-events', '/about/faq/', '/oma-events/past-events', '/about/newsletter'],
       ignore: ['/www.ericsson.com'], 
       failOnError: false,
     },
