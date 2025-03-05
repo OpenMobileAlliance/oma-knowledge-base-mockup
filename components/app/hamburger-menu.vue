e0e78151cd98c793efdc3824c72702bcaa964359
Filip Dragović <dr.ipi95@gmail.com>
2025-03-05 09:27:21 +0100

    update: hover color

--- components/app/hamburger-menu.vue
+++ components/app/hamburger-menu.vue
@@ -73,13 +73,13 @@ const route = useRoute()
 
 const config = {
     rootMenuButton:
-        'w-full flex items-center justify-start px-3 py-2 text-left hover:bg-white dark:hover:bg-gray-800 rounded-lg',
+        'w-full flex items-center justify-start px-3 py-2 text-left hover:bg-gray-200 dark:hover:bg-gray-600 rounded-lg',
     rootMenuLabel: 'font-medium text-xl text-gray-900 dark:text-gray-100',
     rootActive:
         'relative after:content-[" "] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[3px] after:bg-oma-yellow-500 after:rounded-full',
     submenuActive: 'underline underline-offset-4 decoration-2 decoration-oma-yellow-500',
     button:
-        'w-full flex items-center px-4 py-2 text-left hover:bg-gray-200 dark:hover:bg-gray-600 transition duration-200',
+        'w-full flex items-center px-4 py-2 text-left hover:bg-white dark:hover:bg-gray-800 transition duration-200 rounded-lg',
     label: 'font-small text-gray-900 dark:text-gray-100 truncate',
     chevronIcon: 'ml-auto',
 }
